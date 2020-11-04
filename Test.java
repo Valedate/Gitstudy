267d98770ce83e6833c30c18b26b15bc0a104ee7
Valedate <58189910+Valedate@users.noreply.github.com>
2020-11-04 11:31:28 +0100

    Made changes

--- Test.java
+++ Test.java
@@ -2,6 +2,10 @@ public class Test{
 	public static void main(String[] args) {
 				
 		System.out.println("Test");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Test" + i);
+			
+		}
 		
 	}
 }
